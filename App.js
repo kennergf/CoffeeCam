f61fb67e3a72434dd698c0f85391ec4c9650b52c
Tsendeecct <75545203+Tsendeecct@users.noreply.github.com>
2021-04-25 01:09:53 +0100

    changeFlashMode

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import React from 'react';
 import { CameraRoll, StyleSheet, Text, View } from 'react-native';
 import * as ImagePicker from 'expo-image-picker'
 
+
+
 export default class App extends React.Component {
   render() {
     return (
@@ -48,6 +50,26 @@ pickImage = async () => {
   let result = await ImagePicker.launchImageLibraryAsync({
     mediaTypes: ImagePicker.MediaTypeOptions.Images
   });
+}
+
+var flashMode = Observable();
+
+function nextFlashMode() {
+  if (flashMode.value == Camera.FLASH_MODE_AUTO) return Camera.FLASH_MODE_ON;
+  else if (flashMode.value == Camera.FLASH_MODE_ON) return Camera.FLASH_MODE_OFF;
+  else if (flashMode.value == Camera.FLASH_MODE_OFF) return Camera.FLASH_MODE_AUTO;
+  else throw "Invalid flash mode";
+}
+
+function changeFlashMode() {
+  Camera.setFlashMode(nextFlashMode())
+      .then(function(newFlashMode) {
+          flashMode.value = newFlashMode;
+          console.log("Flash mode set to: " + flashMode.value);
+      })
+      .catch(function(err) {
+          console.log("Failed to set flash mode: " + err);
+      });
 }
 
 //
